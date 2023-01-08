eb8429d7e26449956fd5b6e4715314ee4dd048a4
Shevelev <shevelov@gmail.com>
2023-01-08 17:34:41 +0400

    Color palette and color scheme config added, diff row configs for port and horiz window orientation made, random portions (tube content) is generated

---
+++ src/app/configs/Colors.ts
@@ -0,0 +1,41 @@
+/* eslint-disable prettier/prettier */
+export const WhiteBackground = "#ffffff";
+export const BlackBackground = "#000000";
+
+export const TubesStyle = {
+    lineStyle: {
+        width: 1,
+        color: 0xffffff,
+        alpha: 1,
+    },
+    fillStyle: {
+        color: 0xffffff,
+        alpha: 1,
+    },
+}
+
+export const PortionsStyle = {
+    lineStyle: {
+        width: 1,
+        color: 0xffffff,
+        alpha: 1,
+    },
+    fillStyle: {
+        color: 0xffffff,
+        alpha: 0,
+    },
+}
+
+export const AoccPalette = [
+    0xfad201 /* traffic yellow */,
+    0xff7514 /* pastel orange */,
+    0xcc0605 /* traffic red */,
+    0xf6f6f6 /* traffic white */,
+    0x1e1e1e /* traffic black */,
+    0x84c3be /* light green */,
+    0x82898f /* telegrey 2 */,
+    0x57a639 /* yellow green */,
+    0x3b83bd /* light blue */,
+    0x6c4675 /* blue lilac */,
+    0xea899a /* light pink */,
+]
--- src/app/views/GameView.ts
+++ src/app/views/GameView.ts
@@ -1,10 +1,15 @@
 // import { IocContext } from "power-di";
 // import { PopupService } from "../services/PopupService";
+import * as COLORS from "../configs/Colors";
 
 export class GameView extends Phaser.GameObjects.Container {
     // max number of tubes for each row
-    private readonly ROWS = [5, 14, 21, 36];
-    private readonly MAX_TUBES = this.ROWS[this.ROWS.length - 1];
+    private readonly MAX_TUBES = 40;
+    private readonly HOR_ROWS = [5, 16, 24, this.MAX_TUBES];
+    private readonly PORT_ROWS = [4, 10, 18, 28, this.MAX_TUBES];
+    private tubeRows: Array<number>;
+
+    private readonly portionSizeCoeff = 0.8;
 
     // private popupService = IocContext.DefaultInstance.get(PopupService);
     private wi: number;
@@ -19,11 +24,12 @@ export class GameView extends Phaser.GameObjects.Container {
         super(scene);
         this.init();
 
-        this.drawTubes(33, 3);
+        this.drawTubes(10, 5);
     }
 
     private init(): void {
         this.resize();
+        this.tubeRows = this.wi > this.he ? this.HOR_ROWS : this.PORT_ROWS;
 
         this.scene.scale.on("resize", this.resize, this);
     }
@@ -33,9 +39,11 @@ export class GameView extends Phaser.GameObjects.Container {
             console.warn("Tubes num is bigger then max allowed. Setting it to max");
             num = this.MAX_TUBES;
         }
-        let rows = this.ROWS.length;
-        let i = this.ROWS.length - 2;
-        while (num < this.ROWS[i]) {
+
+        // deciding how many rows are needed
+        let rows = this.tubeRows.length;
+        let i = this.tubeRows.length - 2;
+        while (num < this.tubeRows[i]) {
             rows = i;
             i--;
         }
@@ -61,22 +69,45 @@ export class GameView extends Phaser.GameObjects.Container {
         // const portionSquareSize = this.wi > this.he ? Math.round(this.he / 20) : Math.round(this.he / 20);
 
         const tube = this.scene.add.graphics();
-        tube.setDefaultStyles({
-            lineStyle: {
-                width: 1,
-                color: 0xffffff,
-                alpha: 1,
-            },
-            fillStyle: {
-                color: 0xffffff,
-                alpha: 1,
-            },
-        });
+        tube.setDefaultStyles(COLORS.TubesStyle);
         const tubeWi = this.portionSquareSize;
         const tubeHe = this.portionSquareSize * volume;
-        tube.strokeRoundedRect(centerX - tubeWi / 2, centerY - tubeHe / 2, tubeWi, tubeHe, this.portionSquareSize / 5);
+        tube.strokeRoundedRect(0, 0, tubeWi, tubeHe, this.portionSquareSize / 5);
+        tube.setPosition(centerX - tubeWi / 2, centerY - tubeHe / 2);
+        this.fillTube(tube, volume);
 
         this.tubes.push(tube);
+    }
+
+    private fillTube(tube: Phaser.GameObjects.Graphics, volume: number): void {
+        let portion: Phaser.GameObjects.Graphics;
+        let randomColor: number;
+        for (let i = 0; i < volume; i++) {
+            portion = this.scene.add.graphics();
+            portion.setDefaultStyles(COLORS.PortionsStyle);
+            randomColor = COLORS.AoccPalette[Math.floor(Math.random() * COLORS.AoccPalette.length)];
+            portion.fillStyle(randomColor, 1);
+            // this.drawPortionRoundedRect(portion, tube.x, tube.y, i);
+            this.drawPortionCircle(portion, tube.x, tube.y, i);
+        }
+    }
+
+    private drawPortionRoundedRect(figure: Phaser.GameObjects.Graphics, x: number, y: number, num: number): void {
+        figure.fillRoundedRect(
+            x + this.portionSquareSize * 0.1,
+            y + this.portionSquareSize * 0.1 + this.portionSquareSize * num,
+            this.portionSquareSize * this.portionSizeCoeff,
+            this.portionSquareSize * this.portionSizeCoeff,
+            this.portionSquareSize / 6,
+        );
+    }
+
+    private drawPortionCircle(figure: Phaser.GameObjects.Graphics, x: number, y: number, num: number): void {
+        figure.fillCircle(
+            x + this.portionSquareSize * 0.5,
+            y + this.portionSquareSize * (num + 0.5),
+            (this.portionSquareSize * this.portionSizeCoeff) / 2,
+        );
     }
 
     private resize(): void {
