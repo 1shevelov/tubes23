d0105780d465574efb29d1680a6668daeb760115
Shevelev <shevelov@gmail.com>
2023-01-08 09:57:21 +0400

    drawTubes made

--- src/app/views/GameView.ts
+++ src/app/views/GameView.ts
@@ -2,9 +2,15 @@
 // import { PopupService } from "../services/PopupService";
 
 export class GameView extends Phaser.GameObjects.Container {
+    // max number of tubes for each row
+    private readonly ROWS = [5, 14, 21, 36];
+    private readonly MAX_TUBES = this.ROWS[this.ROWS.length - 1];
+
     // private popupService = IocContext.DefaultInstance.get(PopupService);
     private wi: number;
     private he: number;
+
+    private portionSquareSize: number;
 
     // all tubes in the scene
     private tubes: Array<Phaser.GameObjects.Graphics> = [];
@@ -12,7 +18,8 @@ export class GameView extends Phaser.GameObjects.Container {
     public constructor(public scene: Phaser.Scene) {
         super(scene);
         this.init();
-        this.drawTube(this.wi / 2, this.he / 2, 5);
+
+        this.drawTubes(33, 3);
     }
 
     private init(): void {
@@ -21,8 +28,37 @@ export class GameView extends Phaser.GameObjects.Container {
         this.scene.scale.on("resize", this.resize, this);
     }
 
+    private drawTubes(num: number, volume: number): void {
+        if (num > this.MAX_TUBES) {
+            console.warn("Tubes num is bigger then max allowed. Setting it to max");
+            num = this.MAX_TUBES;
+        }
+        let rows = this.ROWS.length;
+        let i = this.ROWS.length - 2;
+        while (num < this.ROWS[i]) {
+            rows = i;
+            i--;
+        }
+        const unevenTubes = num % rows;
+        const tubesInRows = Array(rows).fill((num - unevenTubes) / rows);
+        for (let i = 1; i <= unevenTubes; i++) {
+            tubesInRows[i - 1]++;
+        }
+        // console.log(tubesInRows);
+        const rowGap = this.he / (rows + 1);
+        this.portionSquareSize = rowGap / (volume + 1.5);
+
+        let tubeGap = 0;
+        tubesInRows.forEach((tubesInThisRow, row) => {
+            tubeGap = this.wi / (tubesInThisRow + 1);
+            for (let i = 1; i <= tubesInThisRow; i++) {
+                this.drawTube(i * tubeGap, (row + 1) * rowGap, volume);
+            }
+        });
+    }
+
     private drawTube(centerX: number, centerY: number, volume: number): void {
-        const portionSquareSize = this.wi > this.he ? Math.round(this.he / 20) : Math.round(this.he / 20);
+        // const portionSquareSize = this.wi > this.he ? Math.round(this.he / 20) : Math.round(this.he / 20);
 
         const tube = this.scene.add.graphics();
         tube.setDefaultStyles({
@@ -36,9 +72,9 @@ export class GameView extends Phaser.GameObjects.Container {
                 alpha: 1,
             },
         });
-        const tubeWi = portionSquareSize;
-        const tubeHe = portionSquareSize * volume;
-        tube.strokeRoundedRect(centerX - tubeWi / 2, centerY - tubeHe / 2, tubeWi, tubeHe, portionSquareSize / 5);
+        const tubeWi = this.portionSquareSize;
+        const tubeHe = this.portionSquareSize * volume;
+        tube.strokeRoundedRect(centerX - tubeWi / 2, centerY - tubeHe / 2, tubeWi, tubeHe, this.portionSquareSize / 5);
 
         this.tubes.push(tube);
     }
