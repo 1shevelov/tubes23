d9b43959286bf6472d9941c2df11c4316fa518b8
Shevelev <shevelov@gmail.com>
2023-02-26 22:29:28 +0400

    New Game form ready

--- src/app/views/UIView.ts
+++ src/app/views/UIView.ts
@@ -90,60 +90,25 @@ export class UIView extends Phaser.GameObjects.Container {
             .dom(this.wi / 2, 600)
             .createFromCache("NewGameForm");
         element.setPerspective(800);
-        element.addListener("click");
-        element.on("click", (event) => {
-            if (event.target.name === "StartButton") {
-                const inputUsername = element.getChildByName("username");
-                const inputPassword = element.getChildByName("password");
-
-                //  Have they entered anything?
-                if (inputUsername["value"] !== "" && inputPassword["value"] !== "") {
-                    //  Turn off the click events
-                    this.removeListener("click");
-
-                    //  Tween the login form out
-                    this.scene.tweens.add({
-                        targets: element.rotate3d,
-                        x: 1,
-                        w: 90,
-                        duration: 3000,
-                        ease: "Power3",
-                    });
-
-                    this.scene.tweens.add({
-                        targets: element,
-                        scaleX: 2,
-                        scaleY: 2,
-                        y: 700,
-                        duration: 3000,
-                        ease: "Power3",
-                        onComplete: () => {
-                            element.setVisible(false);
-                        },
-                    });
-
-                    //  Populate the text with whatever they typed in as the username!
-                    // text.setText("Welcome " + inputUsername["value"]);
-                    console.log("Welcome " + inputUsername["value"]);
-                } else {
-                    //  Flash the prompt
-                    // this.scene.tweens.add({
-                    //     targets: text,
-                    //     alpha: 0.1,
-                    //     duration: 200,
-                    //     ease: "Power3",
-                    //     yoyo: true,
-                    // });
-                    console.log("Fill the form!");
-                }
-            }
-        });
 
         this.scene.tweens.add({
             targets: element,
             y: 300,
             duration: 3000,
             ease: "Power3",
+        });
+
+        // element.on("submit", (event) => {
+        //     const form = element.getChildByName("form");
+        //     const data = new FormData(form as HTMLFormElement);
+        //     console.log("Form data: ", data);
+        //     event.preventDefault();
+        // });
+        const form = element.getChildByName("form");
+        (form as HTMLFormElement).addEventListener("submit", (event) => {
+            const data = new FormData(form as HTMLFormElement);
+            console.log("Form data: ", data);
+            event.preventDefault();
         });
     }
 
